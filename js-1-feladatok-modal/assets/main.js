02ac4f00cfedf604ef9b4f8081e10b8c2494b7c5
Geriii <k.geri6@gmail.com>
2021-10-31 08:08:04 +0100

    fix: clicking on modal now doesnt close the modal, only background or buttons can close it, and modal opens unlimited times

--- js-1-feladatok-modal/assets/main.js
+++ js-1-feladatok-modal/assets/main.js
@@ -3,17 +3,24 @@ function modalTrigger(){
     for (var i = 0; i < modalTriggers.length; i++) {
         modalTriggers[i].addEventListener('click', modalOpener);
     }
-}
+};
 modalTrigger();
 function modalOpener(){
     document.querySelector('.container').classList.add("container--visible");
-}
+};
 
 document.querySelector(".container").addEventListener('click', modalCloser);
 document.querySelector(".fa-times").addEventListener('click', modalCloser);
 document.querySelector(".btn-ok").addEventListener('click', modalCloser);
 document.querySelector(".btn-cancel").addEventListener('click', modalCloser);
-
+document.querySelector(".container__modal").addEventListener('click', modalClick);
 function modalCloser(){
     document.querySelector('.container').classList.remove("container--visible");
-}
+    
+};
+function modalClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    return false;
+  };
