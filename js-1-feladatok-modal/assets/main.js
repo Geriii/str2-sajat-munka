4e93f2885d129e68e6f907af2dc00b4f3681fe72
Geriii <k.geri6@gmail.com>
2021-10-31 07:51:05 +0100

    JS functions added, fadein, fadeout added

--- js-1-feladatok-modal/assets/main.js
+++ js-1-feladatok-modal/assets/main.js
@@ -1,4 +1,19 @@
-const modalTrigger = () => document.getElementsByClassName('.trigger').addEventListener('click', modalOpener);
+let modalTriggers = document.querySelectorAll(".trigger")
+function modalTrigger(){
+    for (var i = 0; i < modalTriggers.length; i++) {
+        modalTriggers[i].addEventListener('click', modalOpener);
+    }
+}
+modalTrigger();
 function modalOpener(){
-    document.querySelector('.container').style.display = "block";
+    document.querySelector('.container').classList.add("container--visible");
+}
+
+document.querySelector(".container").addEventListener('click', modalCloser);
+document.querySelector(".fa-times").addEventListener('click', modalCloser);
+document.querySelector(".btn-ok").addEventListener('click', modalCloser);
+document.querySelector(".btn-cancel").addEventListener('click', modalCloser);
+
+function modalCloser(){
+    document.querySelector('.container').classList.remove("container--visible");
 }
